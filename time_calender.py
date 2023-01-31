6c9912ab84baf7fbbce7f84a970b3c7798ee62e8
Hinn <hduc27726@gmail.com>
2023-01-30 16:00:36 -0800

    Create time_calendar.
    Hiển thị lịch theo yêu cầu và thời gian hiện tại của

---
+++ time_calender.py
@@ -0,0 +1,12 @@
+import calendar
+import time
+
+year = int(input("Nam: "))
+month = int(input("Thang: "))
+
+t = time.localtime(time.time())
+localtime = time.asctime(t)
+str = time.asctime(t)
+
+print(calendar.month(year, month))
+print(str)
