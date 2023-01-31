af2d11d8e96840095a73e32b34ef6a4e834e92e6
Hinn <hduc27726@gmail.com>
2023-01-30 16:15:04 -0800

    Update time_calender.py

--- time_calender.py
+++ time_calender.py
@@ -1,12 +1,12 @@
 import calendar
 import time
 
-year = int(input("Nam: "))
-month = int(input("Thang: "))
+year=int(input("Nam: "))
+month=int(input("Thang: "))
 
-t = time.localtime(time.time())
-localtime = time.asctime(t)
-str = time.asctime(t)
+t=time.localtime(time.time())
+localtime=time.asctime(t)
+str=time.asctime(t)
 
 print(calendar.month(year, month))
 print(str)
